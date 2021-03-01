37ed2271a356609c51711317072fa2608be5ccf8
olaulvas <ola.ulvaas@gmail.com>
2021-03-01 12:16:55 +0100

    Initial commit

---
+++ src/test/java/se/lexicon/trying_spring_boot/TryingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.trying_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
